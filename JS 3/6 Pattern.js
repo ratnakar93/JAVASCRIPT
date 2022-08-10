140a4b5fce50f6572b7f47305c3cd7768377f5f8
Ratnakar Singh <82202729+ratnakar93@users.noreply.github.com>
2022-08-10 11:45:38 +0530

    JS 3

---
+++ JS 3/6 Pattern.js
@@ -0,0 +1,16 @@
+/*
+Print the Pattern.
+Write a program which ask user for no of lines and print a pattern using an asterik .
+Note: You have to complete Print_pattern. No need to take any input.
+*/
+
+const Print_pattern = (N) => {
+    for (let row = 1; row <= N; row++) {
+      // console.log("*".repeat(index));
+      let pattern = "";
+      for (let i = 0; i < row; i++) pattern += "*";
+      console.log(pattern);
+    }
+  };
+  
+  Print_pattern(5);
